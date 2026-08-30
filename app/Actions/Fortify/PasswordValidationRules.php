3638d351b0c956dfa2146903afcfea2f7a51c8cb
giorgio <giorgiog_95@hotmail.it>
2026-08-30 15:34:41 +0200

    OWASP7

--- app/Actions/Fortify/PasswordValidationRules.php
+++ app/Actions/Fortify/PasswordValidationRules.php
@@ -13,6 +13,10 @@ trait PasswordValidationRules
      */
     protected function passwordRules(): array
     {
-        return ['required', 'string', Password::default(), 'confirmed'];
+        return ['required', 'string', Password::min(12)
+                                    ->letters()
+                                    ->numbers()
+                                    ->symbols()
+                                    ->uncompromised(),'confirmed'];
     }
 }
